96c3bbb50b0e19765fc2bb6becf72c450732cef1
2363679005@qq.com <2363679005@qq.com>
2021-07-17 15:40:44 +0800

    Update SingleA.java
    饿汉式补全

--- javaLib/src/main/java/com/lib/java/single/SingleA.java
+++ javaLib/src/main/java/com/lib/java/single/SingleA.java
@@ -8,8 +8,8 @@ package com.lib.java.single;
  * 饿汉式单例模式，类加载时已经进行初始化，就已经实例化对象了，在被外部调用时，对象已存在，线程保证了安全性
  */
 public class SingleA {
-    public static SingleA singleA = new SingleA();
-
+    private static SingleA singleA = new SingleA();
+    private SingleA(){}
     public static SingleA getInstance() {
         return singleA;
     }

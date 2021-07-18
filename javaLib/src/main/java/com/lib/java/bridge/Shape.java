76e6116b59667cd825a7ab78cb568d9d65565c6b
2363679005@qq.com <2363679005@qq.com>
2021-07-18 21:08:41 +0800

    解释桥接模式

--- javaLib/src/main/java/com/lib/java/bridge/Shape.java
+++ javaLib/src/main/java/com/lib/java/bridge/Shape.java
@@ -5,6 +5,7 @@ package com.lib.java.bridge;
  * date    :2021/7/18
  * package :com.lib.java.bridge
  * desc    :图形抽象类，持有颜色接口对象
+ * 抽象类持有接口对象，接口对象由构造器赋值，并且自身不实现接口,定义抽象方法由子类进行实现，方法名随意
  */
 public abstract class Shape {
     protected Color color;
--- javaLib/src/main/java/com/lib/java/bridge/Square.java
+++ javaLib/src/main/java/com/lib/java/bridge/Square.java
@@ -5,6 +5,7 @@ package com.lib.java.bridge;
  * date    :2021/7/18
  * package :com.lib.java.bridge
  * desc    :扩充抽象类
+ * 该实体实现父类定义的抽象方法，在该方法内使用父类成员变量的接口类对象去调用接口的方法
  */
 public class Square extends Shape {
 

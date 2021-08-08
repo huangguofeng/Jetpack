2c423c17e70f46d6252afbe988dbab4a9f2a5a8b
2363679005@qq.com <2363679005@qq.com>
2021-08-08 17:00:55 +0800

    声明aidl参数tag为out时，需要在实体类中新增readFromParcel方法，内部实现与有参构造器一致 因为aidl被编译成java文件后 代码中会调用实体类对象的该方法 但是该方法不存在 需要自己实现

--- androidbase/src/main/java/com/example/androidbase/ipc/aidl/BankCard.java
+++ androidbase/src/main/java/com/example/androidbase/ipc/aidl/BankCard.java
@@ -56,4 +56,16 @@ public class BankCard implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeInt(balance);
     }
+
+    /**
+     * 参数是一个Parcel,用它来存储与传输数据
+     * 当aidl接口参数tag属性为out或者inout时，必须创建该方法，代码内部和有参构造器一致
+     * 因为aidl被编译成java文件后 代码中会调用该方法 但是该方法不存在 需要自己实现
+     *
+     * @param dest dest
+     */
+    public void readFromParcel(Parcel dest) {
+        //注意，此处的读值顺序应当是和writeToParcel()方法中一致的
+        balance = dest.readInt();
+    }
 }
--- androidbase/src/main/java/com/example/androidbase/ipc/aidl/User.java
+++ androidbase/src/main/java/com/example/androidbase/ipc/aidl/User.java
@@ -68,4 +68,17 @@ public class User implements Parcelable {
         dest.writeString(name);
         dest.writeParcelable(card, flags);
     }
+
+    /**
+     * 参数是一个Parcel,用它来存储与传输数据
+     * 当aidl接口参数tag属性为out或者inout时，必须创建该方法，代码内部和有参构造器一致
+     * 因为aidl被编译成java文件后 代码中会调用该方法 但是该方法不存在 需要自己实现
+     *
+     * @param dest dest
+     */
+    public void readFromParcel(Parcel dest) {
+        //注意，此处的读值顺序应当是和writeToParcel()方法中一致的
+        name = dest.readString();
+        card = dest.readParcelable(BankCard.class.getClassLoader());
+    }
 }

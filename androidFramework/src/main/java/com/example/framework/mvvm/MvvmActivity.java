ebd78137f554d73186c8eac6c2542b0aa104e5a9
2363679005@qq.com <2363679005@qq.com>
2021-08-11 20:09:57 +0800

    ...

--- androidFramework/src/main/java/com/example/framework/mvvm/MvvmActivity.java
+++ androidFramework/src/main/java/com/example/framework/mvvm/MvvmActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.framework.R;
 import com.example.framework.databinding.ListViewItemBinding;
@@ -16,7 +17,7 @@ import com.lib.base.databind.BaseBindingComponent;
  * View角色
  */
 public class MvvmActivity extends AppCompatActivity {
-    private NewsModel model = new NewsModel();
+    private NewsModel model;
     ListViewItemBinding binding;
 
     @Override
@@ -24,7 +25,7 @@ public class MvvmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.list_view_item, new BaseBindingComponent());
-
+        model = new ViewModelProvider(this).get(NewsModel.class);
         model.data.observe(this, new Observer<News>() {
             @Override
             public void onChanged(News news) {
